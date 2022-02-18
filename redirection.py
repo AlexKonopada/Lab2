96f2e7766ea87bb12b69970cab6bf206372d4162
AlexKonopada <96167317+AlexKonopada@users.noreply.github.com>
2022-02-18 23:24:00 +0200

    Update redirection.py

--- redirection.py
+++ redirection.py
@@ -1,16 +1,20 @@
+import twitter2
+from lab2_3 import main
 from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'] )
 def home():
-    return render_template('lab2_2.html')
-
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
     if request.method == 'POST':
         user = request.form['nm']
-        return user
+        return redirect(url_for('create_map', usr = user ))
     else:
         return render_template('lab2_2.html')
+
+@app.route('/<usr>')
+def create_map(usr):
+    main(usr)
+    
+    return render_template('tavarischi.html')
 if __name__ == '__main__':
     app.run()
