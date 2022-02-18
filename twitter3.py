2c6208368a74adade7eab84ff2676bd88ff16212
AlexKonopada <96167317+AlexKonopada@users.noreply.github.com>
2022-02-18 23:37:48 +0200

    Add files via upload

---
+++ twitter3.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import json
+import ssl
+
+
+
+TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+def data_from_twitter3():
+    acct = input('Enter Twitter Account:')
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': acct, 'count': '5'})
+    connection = urllib.request.urlopen(url, context=ctx)
+    data = connection.read().decode()
+
+    js = json.loads(data)
+    return js
